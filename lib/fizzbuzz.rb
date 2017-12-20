306386eea18d5651fa980755daca1d253cf5b41f
Reena <reena.sanjay.sharma@gmail.com>
2017-12-20 17:23:55 +0000

    added all the test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if (number % 3 == 0) and (number % 5 == 0)
+    "fizzbuzz" 
+  elsif number % 3 == 0
     "fizz"
   elsif number % 5 == 0
     "buzz"
+  else
+    number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,10 @@ describe 'fizzbuzz'do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+end
 end
